2954f45ce8052203db844e961355a3a3ca48c48c
Gabriel Miretti <gabriel.miretti@gmail.com>
2021-04-16 18:50:58 -0300

    Solucion inicial de automatizacion

--- README.md
+++ README.md
@@ -4,24 +4,97 @@
 
 Proyecto inicial para realizar speed-up de un código de Dinámica Molecular con un potencial interatómico de Lennard-Jones. Inicialmente se dan las posiciones en un cristal FCC y velocidades aleatorias distribuidas uniformemente según la temperatura inicial. Las fuerzas se obtienen de un potencial de LJ (12-6) y la evolución temporal viene dada por el algoritmo Velocity Verlet. Se consideran condiciones periódicas de contorno para reproducir un sistema infinito. La temperatura es controlada a través de un reescaleo en las velocidades. Cada cierta cantidad de pasos de dinámica molecular se cambia la densidad del sistema y se reescalean las posiciones para obtener la ecuación de estado. Para más información se puede ver `TinyMD.pdf`.
 
+## Prerequisitos
 
-### Requisitos
+* python>=3.6
 
-Para compilar es necesario tener instalado `gcc` y `OpenGL`.
+    `sudo apt install python3 python3-pip` en Ubuntu 18.04
+
+* invoke==1.5.0
+  * recomendación instalar con [pipx](https://pipxproject.github.io/pipx/installation/) como user (sin `sudo`), el paquete `python3-invoke` suele estar desactualizado
+
+    ```bash
+    python3 -m pip install --user pipx
+    python3 -m pipx ensurepath
+    pipx install invoke==1.5.0
+    pipx inject invoke python-slugify
+    ```
+  
+* perf
+  * `sudo apt install --install-recommends linux-generic-hwe-18.04 xserver-xorg-hwe-18.04 linux-tools-generic-hwe-18.04` en Ubuntu 18.04 para usar el último kernel.
+  
+    Sino `sudo apt install linux-tools-generic`
+  * También es necesario que `cat /proc/sys/kernel/perf_event_paranoid` sea -1.
+
+    Si no es -1, hacer `echo -1 | sudo tee /proc/sys/kernel/perf_event_paranoid`
+
+### Hyperthreading
+
+Verificar si Hyperthreading funciona
+
+    cat /sys/devices/system/cpu/smt/active
+
+Apagar hyperthreading
+
+    echo off | sudo tee /sys/devices/system/cpu/smt/control
+
+Volver a prender
+
+    echo on | sudo tee /sys/devices/system/cpu/smt/control
 
 
-### Compilación
+## Como correr soluciones
+
+El proyecto esta estructurado en soluciones (`solutions` dir). Cada solucion tiene su carpeta y dentro de esta su fuente (`src`), sus binarios executables (`bin`) y sus registros (`log`), estos 2 últimos son guardados a su vez en una carpeta por máquina
+
+    solutions/$name/src
+    solutions/$name/bin/$machine/
+    solutions/$name/log/$machine/
+
+El código original está en la solución `original`
+
+Para crear una nueva solución basada en la original
+
+    invoke create-solution $name
+
+Nota: se puede agregar `--template=$existint_solution_name` para copiar de otra solucion que no sea la original
+
+Para ejecutar una solucion
+
+    invoke run --solution $name
+
+Esto compila creando un nuevo ejecutable y lo ejecuta.
+
+Para ver todas las opciones de ejecucion mirar:
+
+    invoke run --help
+
+Para ver todos los comandos hacer
+
+    invoke --list
+
+## Requisitos
+
+Para compilar es necesario tener instalado:
+
+* `gcc`
+* `OpenGL`
+  * Instalar con `sudo apt install freeglut3-dev` en Ubuntu 18.04
+
+## Compilación
 
 Para compilar se utiliza `Makefile`:
+
 ```bash
 make clean
 make
 ```
+
 donde `make clean` elimina los objetos compilados anteriormente y `make` compila dos ejecutables: `tiny_md` y `viz`, ambos realizan la misma simulación pero el segundo posee una visualización en tiempo real.
 
 > Nota:
 >
-> _Si se desean cambiar parámetros de entrada de la simulación, puede modificarse el archivo _`parameters.h`_ o pasar los valores deseados como parámetros al preprocesador C; por ejemplo, _`make CPPFLAGS="-DN=1372"`_ cambia la cantidad de partículas que se simulan._
+> _Si se desean cambiar parámetros de entrada de la simulación, puede modificarse el archivo _`parameters.h`_ o pasar los valores deseados como parámetros al preprocesador C; por ejemplo, _`make PARAMFLAGS="-DN=1372"`_ cambia la cantidad de partículas que se simulan._
 
 ### Archivos
 *Quizás darle otra ubicación a esta lista*
@@ -38,6 +111,6 @@ donde `make clean` elimina los objetos compilados anteriormente y `make` compila
 * wtime.h : `#pragma once`
 ### Contacto
 
-Por errores, preguntas o sugerencias contactarse con:
+Por errores, preguntas o sugerencias contactarse con autor original :D
 
 + Francisco Fernandez (<fernandezfrancisco2195@gmail.com>)
---
+++ tasks.py
@@ -0,0 +1,198 @@
+from datetime import datetime
+from pathlib import Path
+import platform
+from invoke import task
+from slugify import slugify
+
+SOLUTIONS_DIR = Path('./solutions')
+DEFAULT_SOLUTION = 'original'
+BIN_DIR = 'bin'
+SRC_DIR = 'src'
+LOG_DIR = 'log'
+MACHINE_NAME = slugify(platform.node())
+DEFAULT_CC = "gcc"
+DEFAULT_CFLAGS = "-O0"
+DEFAULT_LDFLAGS = "-lm"
+DEFAULT_PARAMFLAGS = "-DN256"
+DEFAULT_WFLAGS = "-std=gnu11 -Wall -Wextra -g"
+CSV_SEPARATOR = ","
+DEFAULT_METRIC = "GFLOPs"
+DEFAULT_RETRIES = 3
+
+
+def slugify_iter(*args):
+    return slugify(' '.join(args))
+
+
+def get_solution_dir(solution):
+    sol_dir = SOLUTIONS_DIR / solution
+    if not sol_dir.exists():
+        print("Creating dir %s" % sol_dir)
+        sol_dir.mkdir(parents=True, exist_ok=True)
+    return sol_dir
+
+
+def get_src_dir(solution):
+    src_dir = SOLUTIONS_DIR / solution / SRC_DIR
+    if not src_dir.exists():
+        print("Creating dir %s" % src_dir)
+        src_dir.mkdir(parents=True, exist_ok=True)
+    return src_dir
+
+
+def get_exec_dir(solution):
+    exec_dir = SOLUTIONS_DIR / solution / BIN_DIR / MACHINE_NAME
+    if not exec_dir.exists():
+        print("Creating dir %s" % exec_dir)
+        exec_dir.mkdir(parents=True, exist_ok=True)
+    return exec_dir
+
+
+def get_log_dir(solution):
+    log_dir = SOLUTIONS_DIR / solution / LOG_DIR / MACHINE_NAME
+    if not log_dir.exists():
+        print("Creating dir %s" % log_dir)
+        log_dir.mkdir(parents=True, exist_ok=True)
+    return log_dir
+
+
+def get_exec_name(cc, cflags, ldflags, paramflags, name="tiny_md"):
+    return slugify_iter(name, cc, cflags, ldflags, paramflags)
+
+
+def get_log_prefix(exec_name, start_time=datetime.now()):
+    return "%s-%s" % (start_time.isoformat(timespec='minutes'), exec_name)
+
+
+def get_flags_str(cc, cflags, ldflags, paramflags, wflags):
+    return 'CC="%s" CFLAGS="%s" LDFLAGS="%s" PARAMFLAGS="%s" WFLAGS="%s"' % (cc, cflags, ldflags, paramflags, wflags)
+
+
+@task()
+def clean_src(
+        c,
+        solution=DEFAULT_SOLUTION
+        ):
+    """Clean solution source"""
+    src_dir = get_src_dir(solution)
+    with c.cd(src_dir):
+        print(c.cwd)
+        print("Cleaning!")
+        c.run("make clean")
+        print("Cleaned!")
+
+
+@task()
+def clean_exec(
+        c,
+        solution=DEFAULT_SOLUTION
+        ):
+    """Clean solution executables"""
+    exec_dir = get_exec_dir(solution)
+    c.run(f"rm -R {exec_dir}")
+
+
+@task()
+def clean_log(
+        c,
+        solution=DEFAULT_SOLUTION
+        ):
+    """Clean solution exec logs"""
+    log_dir = get_log_dir(solution)
+    c.run(f"rm -R {log_dir}")
+
+
+@task()
+def clean_solution(
+        c,
+        solution=DEFAULT_SOLUTION
+        ):
+    """Clean solution"""
+    clean_src(c, solution)
+    clean_exec(c, solution)
+    clean_log(c, solution)
+
+
+@task(help={'must_clean': 'make clean is called prior to build it by default'})
+def build(
+        c,
+        must_clean=True,
+        cc=DEFAULT_CC,
+        cflags=DEFAULT_CFLAGS,
+        wflags=DEFAULT_WFLAGS,
+        ldflags=DEFAULT_LDFLAGS,
+        paramflags=DEFAULT_PARAMFLAGS,
+        solution=DEFAULT_SOLUTION
+        ):
+    """Build project with default options"""
+    if must_clean:
+        clean_src(c, solution)
+    src_dir = get_src_dir(solution)
+    with c.cd(src_dir):
+        print(c.cwd)
+        print("Building!")
+        c.run("make tiny_md %s" % get_flags_str(cc, cflags, ldflags, paramflags, wflags))
+        print("Built!")
+    exec_dir = get_exec_dir(solution)
+    exec_path = exec_dir / get_exec_name(cc, cflags, ldflags, paramflags)
+    c.run("mv %s/tiny_md %s" % (src_dir, exec_path))
+    print("Binary moved to %s" % exec_dir)
+
+
+@task()
+def run(
+        c,
+        must_clean=True,
+        cc=DEFAULT_CC,
+        cflags=DEFAULT_CFLAGS,
+        wflags=DEFAULT_WFLAGS,
+        ldflags=DEFAULT_LDFLAGS,
+        paramflags=DEFAULT_PARAMFLAGS,
+        solution=DEFAULT_SOLUTION,
+        metric=DEFAULT_METRIC,
+        retries=DEFAULT_RETRIES
+        ):
+    """
+    Run a *solution* given some parameters recording a *metric* and
+    build it if exec doesn't exist
+    """
+    exec_dir = get_exec_dir(solution)
+    exec_name = get_exec_name(cc, cflags, ldflags, paramflags)
+    exec_path = exec_dir / exec_name
+    if not exec_path.exists():
+        build(c, must_clean, cc, cflags, wflags, ldflags, paramflags, solution)
+    log_dir = get_log_dir(solution)
+    log_path = log_dir / f"{get_log_prefix(exec_name)}.log"
+    stat_csv_path = log_dir / f"{get_log_prefix(exec_name)}-{metric}.csv"
+    command = (f"perf stat -x '{CSV_SEPARATOR}' "
+               "-e 'cs,migrations,faults' "
+               f"-M '{metric}' "
+               f"-r {retries} "
+               f"-o {stat_csv_path} "
+               f"{exec_path}"
+               )
+    print("Running $ %s" % command)
+    print("Look at log doing $ tail -f %s" % log_path)
+    with open(log_path, "a") as log_stream:
+        c.run(
+            command,
+            env={'LC_NUMERIC': 'en_US'},
+            out_stream=log_stream)
+    c.run(
+        f"mv trajectory.xyz {log_dir / get_log_prefix(exec_name)}-trajectory.xyz",
+        warn=True)
+    c.run(
+        f"mv thermo.log {log_dir / get_log_prefix(exec_name)}-thermo.log",
+        warn=True)
+
+
+@task()
+def create_solution(
+        c,
+        name,
+        template=DEFAULT_SOLUTION):
+    "Create a new solution with *name* using a *template* src code dir"
+    new_src_dir = get_solution_dir(name)
+    old_src_dir = get_src_dir(template)
+    c.run(f"cp -R {old_src_dir} {new_src_dir}")
+    clean_src(c, name)
