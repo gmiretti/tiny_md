706419753b59f7a24b9232a88aebcb1e1b694063
Gabriel Miretti <gabriel.miretti@gmail.com>
2021-04-19 14:58:38 -0300

    agregamos run-all-ccs y cambiamos metric option for measure-options

--- tasks.py
+++ tasks.py
@@ -4,24 +4,25 @@ import platform
 from invoke import task
 from slugify import slugify
 
-SOLUTIONS_DIR = Path('./solutions')
-DEFAULT_SOLUTION = 'original'
-BIN_DIR = 'bin'
-SRC_DIR = 'src'
-LOG_DIR = 'log'
+SOLUTIONS_DIR = Path("./solutions")
+DEFAULT_SOLUTION = "original"
+BIN_DIR = "bin"
+SRC_DIR = "src"
+LOG_DIR = "log"
 MACHINE_NAME = slugify(platform.node())
 DEFAULT_CC = "gcc"
+DEFAULT_CCS = ["gcc-5", "gcc-10", "icc", "clang-6.0", "clang-12", "nvc"]
 DEFAULT_CFLAGS = "-O0"
 DEFAULT_LDFLAGS = "-lm"
 DEFAULT_PARAMFLAGS = "-DN256"
 DEFAULT_WFLAGS = "-std=gnu11 -Wall -Wextra -g"
 CSV_SEPARATOR = ","
-DEFAULT_METRIC = "GFLOPs"
+DEFAULT_MEASURE_OPTS = "-e 'cs,migrations,faults' -M 'GFLOPs' "
 DEFAULT_RETRIES = 3
 
 
 def slugify_iter(*args):
-    return slugify(' '.join(args))
+    return slugify(" ".join(args))
 
 
 def get_solution_dir(solution):
@@ -61,18 +62,21 @@ def get_exec_name(cc, cflags, ldflags, paramflags, name="tiny_md"):
 
 
 def get_log_prefix(exec_name, start_time=datetime.now()):
-    return "%s-%s" % (start_time.isoformat(timespec='minutes'), exec_name)
+    return "%s-%s" % (start_time.isoformat(timespec="minutes"), exec_name)
 
 
 def get_flags_str(cc, cflags, ldflags, paramflags, wflags):
-    return 'CC="%s" CFLAGS="%s" LDFLAGS="%s" PARAMFLAGS="%s" WFLAGS="%s"' % (cc, cflags, ldflags, paramflags, wflags)
+    return 'CC="%s" CFLAGS="%s" LDFLAGS="%s" PARAMFLAGS="%s" WFLAGS="%s"' % (
+        cc,
+        cflags,
+        ldflags,
+        paramflags,
+        wflags,
+    )
 
 
 @task()
-def clean_src(
-        c,
-        solution=DEFAULT_SOLUTION
-        ):
+def clean_src(c, solution=DEFAULT_SOLUTION):
     """Clean solution source"""
     src_dir = get_src_dir(solution)
     with c.cd(src_dir):
@@ -83,47 +87,38 @@ def clean_src(
 
 
 @task()
-def clean_exec(
-        c,
-        solution=DEFAULT_SOLUTION
-        ):
+def clean_exec(c, solution=DEFAULT_SOLUTION):
     """Clean solution executables"""
     exec_dir = get_exec_dir(solution)
     c.run(f"rm -R {exec_dir}")
 
 
 @task()
-def clean_log(
-        c,
-        solution=DEFAULT_SOLUTION
-        ):
+def clean_log(c, solution=DEFAULT_SOLUTION):
     """Clean solution exec logs"""
     log_dir = get_log_dir(solution)
     c.run(f"rm -R {log_dir}")
 
 
 @task()
-def clean_solution(
-        c,
-        solution=DEFAULT_SOLUTION
-        ):
+def clean_solution(c, solution=DEFAULT_SOLUTION):
     """Clean solution"""
     clean_src(c, solution)
     clean_exec(c, solution)
     clean_log(c, solution)
 
 
-@task(help={'must_clean': 'make clean is called prior to build it by default'})
+@task(help={"must_clean": "make clean is called prior to build it by default"})
 def build(
-        c,
-        must_clean=True,
-        cc=DEFAULT_CC,
-        cflags=DEFAULT_CFLAGS,
-        wflags=DEFAULT_WFLAGS,
-        ldflags=DEFAULT_LDFLAGS,
-        paramflags=DEFAULT_PARAMFLAGS,
-        solution=DEFAULT_SOLUTION
-        ):
+    c,
+    must_clean=True,
+    cc=DEFAULT_CC,
+    cflags=DEFAULT_CFLAGS,
+    wflags=DEFAULT_WFLAGS,
+    ldflags=DEFAULT_LDFLAGS,
+    paramflags=DEFAULT_PARAMFLAGS,
+    solution=DEFAULT_SOLUTION,
+):
     """Build project with default options"""
     if must_clean:
         clean_src(c, solution)
@@ -131,7 +126,9 @@ def build(
     with c.cd(src_dir):
         print(c.cwd)
         print("Building!")
-        c.run("make tiny_md %s" % get_flags_str(cc, cflags, ldflags, paramflags, wflags))
+        c.run(
+            "make tiny_md %s" % get_flags_str(cc, cflags, ldflags, paramflags, wflags)
+        )
         print("Built!")
     exec_dir = get_exec_dir(solution)
     exec_path = exec_dir / get_exec_name(cc, cflags, ldflags, paramflags)
@@ -141,17 +138,17 @@ def build(
 
 @task()
 def run(
-        c,
-        must_clean=True,
-        cc=DEFAULT_CC,
-        cflags=DEFAULT_CFLAGS,
-        wflags=DEFAULT_WFLAGS,
-        ldflags=DEFAULT_LDFLAGS,
-        paramflags=DEFAULT_PARAMFLAGS,
-        solution=DEFAULT_SOLUTION,
-        metric=DEFAULT_METRIC,
-        retries=DEFAULT_RETRIES
-        ):
+    c,
+    must_clean=True,
+    cc=DEFAULT_CC,
+    cflags=DEFAULT_CFLAGS,
+    wflags=DEFAULT_WFLAGS,
+    ldflags=DEFAULT_LDFLAGS,
+    paramflags=DEFAULT_PARAMFLAGS,
+    solution=DEFAULT_SOLUTION,
+    measure_options=DEFAULT_MEASURE_OPTS,
+    retries=DEFAULT_RETRIES,
+):
     """
     Run a *solution* given some parameters recording a *metric* and
     build it if exec doesn't exist
@@ -163,34 +160,54 @@ def run(
         build(c, must_clean, cc, cflags, wflags, ldflags, paramflags, solution)
     log_dir = get_log_dir(solution)
     log_path = log_dir / f"{get_log_prefix(exec_name)}.log"
-    stat_csv_path = log_dir / f"{get_log_prefix(exec_name)}-{metric}.csv"
-    command = (f"perf stat -x '{CSV_SEPARATOR}' "
-               "-e 'cs,migrations,faults' "
-               f"-M '{metric}' "
-               f"-r {retries} "
-               f"-o {stat_csv_path} "
-               f"{exec_path}"
-               )
+    stat_csv_path = log_dir / f"{get_log_prefix(exec_name)}-{slugify(measure_options)}.csv"
+    command = (
+        f"perf stat -x '{CSV_SEPARATOR}' "
+        f"{measure_options} "
+        f"-r {retries} "
+        f"-o {stat_csv_path} "
+        f"{exec_path}"
+    )
     print("Running $ %s" % command)
     print("Look at log doing $ tail -f %s" % log_path)
     with open(log_path, "a") as log_stream:
-        c.run(
-            command,
-            env={'LC_NUMERIC': 'en_US'},
-            out_stream=log_stream)
+        c.run(command, env={"LC_NUMERIC": "en_US"}, out_stream=log_stream)
     c.run(
         f"mv trajectory.xyz {log_dir / get_log_prefix(exec_name)}-trajectory.xyz",
-        warn=True)
-    c.run(
-        f"mv thermo.log {log_dir / get_log_prefix(exec_name)}-thermo.log",
-        warn=True)
+        warn=True,
+    )
+    c.run(f"mv thermo.log {log_dir / get_log_prefix(exec_name)}-thermo.log", warn=True)
 
 
 @task()
-def create_solution(
-        c,
-        name,
-        template=DEFAULT_SOLUTION):
+def run_all_ccs(
+    c,
+    must_clean=True,
+    cflags=DEFAULT_CFLAGS,
+    wflags=DEFAULT_WFLAGS,
+    ldflags=DEFAULT_LDFLAGS,
+    paramflags=DEFAULT_PARAMFLAGS,
+    solution=DEFAULT_SOLUTION,
+    measure_options=DEFAULT_MEASURE_OPTS,
+    retries=DEFAULT_RETRIES,
+):
+    for cc in DEFAULT_CCS:
+        run(
+            c,
+            must_clean,
+            cc,
+            cflags,
+            wflags,
+            ldflags,
+            paramflags,
+            solution,
+            measure_options,
+            retries,
+        )
+
+
+@task()
+def create_solution(c, name, template=DEFAULT_SOLUTION):
     "Create a new solution with *name* using a *template* src code dir"
     new_src_dir = get_solution_dir(name)
     old_src_dir = get_src_dir(template)
